4cced2697685ddcfc88629e7c1b375f6119b553e
freelancejsdev <apiusertest.5@gmail.com>
2021-04-04 15:03:07 +0100

    cypress

---
+++ cypress/integration/testCases/test1.spec.js
@@ -0,0 +1,17 @@
+describe('nopCommerce',()=>{
+
+     before('navigate to url',()=>{
+
+        cy.visit('https://demo.nopcommerce.com/')
+            
+    })
+
+    it('search for product',()=>{
+          
+    })
+
+    it('',()=>{
+         
+    })
+
+})
